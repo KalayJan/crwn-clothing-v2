c43d9b7493117df9782c4dc327ebcfc4dc073a2f
Yihua Zhang <yihuazhang2@gmail.com>
2022-03-20 20:38:27 -0400

    using createAction

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -1,56 +1,39 @@
 import { USER_ACTION_TYPES } from './user.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
 
-export const setCurrentUser = (user) =>
-  createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
+export const checkUserSession = () =>
+  createAction(USER_ACTION_TYPES.CHECK_USER_SESSION);
 
-export const googleSignInStart = () => ({
-  type: USER_ACTION_TYPES.GOOGLE_SIGN_IN_START,
-});
+export const googleSignInStart = () =>
+  createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
 
-export const signInSuccess = (user) => ({
-  type: USER_ACTION_TYPES.SIGN_IN_SUCCESS,
-  payload: user,
-});
+export const emailSignInStart = (email, password) =>
+  createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password });
 
-export const signInFailed = (error) => ({
-  type: USER_ACTION_TYPES.SIGN_IN_FAILED,
-  payload: error,
-});
+export const signInSuccess = (user) =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
 
-export const emailSignInStart = (email, password) => ({
-  type: USER_ACTION_TYPES.EMAIL_SIGN_IN_START,
-  payload: { email, password },
-});
+export const signInFailed = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
 
-export const checkUserSession = () => ({
-  type: USER_ACTION_TYPES.CHECK_USER_SESSION,
-});
+export const signOutStart = () =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_START);
 
-export const signOutStart = () => ({
-  type: USER_ACTION_TYPES.SIGN_OUT_START,
-});
+export const signOutSuccess = () =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_SUCCESS);
 
-export const signOutSuccess = () => ({
-  type: USER_ACTION_TYPES.SIGN_OUT_SUCCESS,
-});
+export const signOutFailed = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_OUT_FAILED, error);
 
-export const signOutFailed = (error) => ({
-  type: USER_ACTION_TYPES.SIGN_OUT_FAILED,
-  payload: error,
-});
+export const signUpStart = (email, password, displayName) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_START, {
+    email,
+    password,
+    displayName,
+  });
 
-export const signUpStart = (email, password, displayName) => ({
-  type: USER_ACTION_TYPES.SIGN_UP_START,
-  payload: { email, password, displayName },
-});
+export const signUpSuccess = ({ user, additionalData }) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_SUCCESS, { user, additionalData });
 
-export const signUpSuccess = ({ user, additionalData }) => ({
-  type: USER_ACTION_TYPES.SIGN_UP_SUCCESS,
-  payload: { user, additionalData },
-});
-
-export const signUpFailed = (error) => ({
-  type: USER_ACTION_TYPES.SIGN_UP_FAILED,
-  payload: error,
-});
+export const signUpFailed = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_UP_FAILED, error);
